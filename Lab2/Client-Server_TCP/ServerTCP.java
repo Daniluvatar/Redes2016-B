523ae0d9a6534000816d12f8f70a434d5ebe3353
Daniluvatar <daniluvatar@gmail.com>
2016-11-10 10:22:44 -0500

    Update ServerTCP.java

--- Lab2/Client-Server_TCP/ServerTCP.java
+++ Lab2/Client-Server_TCP/ServerTCP.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 
 /**
  * A TCP server that runs on port 9090.  When a client connects, it
- * sends the client the current date and time, then closes the
+ * sends the client a message, then closes the
  * connection with that client.  Arguably just about the simplest
  * server you can write.
  */
@@ -15,7 +15,7 @@ public class ServerTCP {
 
 	private static int PORT = 9090;
 	
-	/**
+     /**
      * Runs the server.
      */
     public static void main(String[] args) throws IOException {
