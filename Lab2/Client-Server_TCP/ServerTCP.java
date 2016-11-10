6635deab7f4727443d50895cf4d91141c06a4442
Daniluvatar <daniluvatar@gmail.com>
2016-11-10 10:22:04 -0500

    Create ServerTCP.java

---
+++ Lab2/Client-Server_TCP/ServerTCP.java
@@ -0,0 +1,42 @@
+package ec.edu.epn.redes.cs.tcp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * A TCP server that runs on port 9090.  When a client connects, it
+ * sends the client the current date and time, then closes the
+ * connection with that client.  Arguably just about the simplest
+ * server you can write.
+ */
+public class ServerTCP {
+
+	private static int PORT = 9090;
+	
+	/**
+     * Runs the server.
+     */
+    public static void main(String[] args) throws IOException {
+        
+    	ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server listening on port "+PORT);
+        
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                try {
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println("Danilo"); //Cambiar por el nombre que corresponda
+                } finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+        	serverSocket.close();
+        }
+    }
+	
+}
